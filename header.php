ecc99c45e54454191adfb1dc9171c0986e34a828
NCMoseley <ncmoseley@gmail.com>
2017-11-28 15:45:36 -0800

    Clean up comments in code

--- header.php
+++ header.php
@@ -13,28 +13,17 @@
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
-
-
-	<?php wp_head(); ?>
+	    <?php wp_head(); ?>
 	</head>
-
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
-
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
-
-				<section class="home-hero">
-
-				<a href="<?php echo get_site_url() ?>" ><img class="site-description" class="button" src="<?php echo get_site_url().'/wp-content/themes/quotesondev/img/qod-logo.svg'; ?>"></img></a>
-    
-                </section>
-
-				<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					
-				</div><!-- .site-branding -->
-
-			</header><!-- #masthead -->
-
-			<div id="content" class="site-content">
+				    <section class="home-hero">
+				      <a href="<?php echo get_site_url() ?>" ><img class="site-description button" alt="Quotes on Dev Logo" src="<?php echo get_site_url().'/wp-content/themes/quotesondev/img/qod-logo.svg'; ?>"></a>
+                    </section>
+				    <h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>			
+				</div>
+			</header>
+	    <div id="content" class="site-content">
